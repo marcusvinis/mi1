75836a4a19a954f26cb30521bea1f681e6b7f20d
Marcus Vincius da Silva <themarcusvinicius@gmail.com>
2017-09-09 02:05:53 -0300

    Add functag
    correçao exibicao de tag automatica nos posts

---
+++ functag.js
@@ -0,0 +1,37 @@
+//Inclui a sigla do console nos posts, no quadro acima da contagem de comentários
+function MontaIdentificacaoConsole(){
+    if($('#tilebar')){
+	var listaTags = $('#marcadores_consoles li');
+	var console = '';
+	jQuery.each(listaTags,function(i,tag){
+		jQuery.each(listaConsoles,function(i,cons){
+			if(tag.innerHTML == cons){
+                if(console.length > 1){
+					console = 'Multi';
+					return false;
+				}
+				else{
+					console += cons;
+				}
+			}
+		});
+	});
+	if(console.length > 1){
+		if(console == 'Multi'){
+			$('#tilebar').prepend("<a class='bigtag' href='#'>"+console+"</a>");
+		}
+		else{
+			if(console == 'PSVita'){
+				console = 'PSV'
+            }
+			if(console == 'Wii U'){
+           $('#tilebar').prepend("<a class='bigtag' href="+urlSite+"/search/label/Wii%20U>"+console+"</a>")
+            }
+            else{
+			$('#tilebar').prepend("<a class='bigtag' href="+urlSite+"/search/label/"+console+">"+console+"</a>");
+            }
+		}
+	}
+
+    }
+}
