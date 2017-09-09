95ad8b6fc44c96e859b02b6c00edcce17cd8211a
Marcus Vincius da Silva <themarcusvinicius@gmail.com>
2017-09-09 02:09:31 -0300

    removido trecho das tags pra outro arquivo
                                    //Inclui a sigla do console nos posts, no quadro acima da contagem de comentários
    function MontaIdentificacaoConsole(){
        if($('#tilebar').length>0){
    	var listaTags = $('#marcadores_consoles li');
    	var console = '';
    	jQuery.each(listaTags,function(i,tag){
    		jQuery.each(listaConsoles,function(i,cons){
    			if(tag.innerHTML == cons){
                    if(console.length > 1){
    					console = 'Multi';
    					return false;
    				}
    				else{
    					console += cons;
    				}
    			}
    		});
    	});
    	if(console.length > 1){
    		if(console == 'Multi'){
    			$('#tilebar').prepend("<a class='bigtag' href='#'>"+console+"</a>");
    		}
    		else{
    			if(console == 'PSVita'){
    				console = 'PSV'
                }
    			if(console == 'Wii U'){
               $('#tilebar').prepend("<a class='bigtag' href="+urlSite+"/search/label/Wii%20U>"+console+"</a>")
                }
                else{
    			$('#tilebar').prepend("<a class='bigtag' href="+urlSite+"/search/label/"+console+">"+console+"</a>");
                }
    		}
    	}
        }
    }

--- montasite.js
+++ montasite.js
@@ -273,42 +273,7 @@ function fixaBarraLateral(){
 	}
 }
 
-                                //Inclui a sigla do console nos posts, no quadro acima da contagem de comentários
-function MontaIdentificacaoConsole(){
-    if($('#tilebar').length>0){
-	var listaTags = $('#marcadores_consoles li');
-	var console = '';
-	jQuery.each(listaTags,function(i,tag){
-		jQuery.each(listaConsoles,function(i,cons){
-			if(tag.innerHTML == cons){
-                if(console.length > 1){
-					console = 'Multi';
-					return false;
-				}
-				else{
-					console += cons;
-				}
-			}
-		});
-	});
-	if(console.length > 1){
-		if(console == 'Multi'){
-			$('#tilebar').prepend("<a class='bigtag' href='#'>"+console+"</a>");
-		}
-		else{
-			if(console == 'PSVita'){
-				console = 'PSV'
-            }
-			if(console == 'Wii U'){
-           $('#tilebar').prepend("<a class='bigtag' href="+urlSite+"/search/label/Wii%20U>"+console+"</a>")
-            }
-            else{
-			$('#tilebar').prepend("<a class='bigtag' href="+urlSite+"/search/label/"+console+">"+console+"</a>");
-            }
-		}
-	}
-    }
-}
+
 //Inicializa as várias funções após o carregamento da página
 $(document).ready(function () {
 	
